82362fa77a88d91f7776ecaca7ee5a7f1cab18a2
ReddixT <63563893+ReddixT@users.noreply.github.com>
2020-05-30 16:05:22 +0200

    SOV can invade Iran after 1941
    - Seal application pic added
    - Building fixes

--- common/defines/CMU_defines.lua
+++ common/defines/CMU_defines.lua
@@ -323,10 +323,10 @@ NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 0
 NDefines_Graphics.NGraphics.BORDER_WIDTH = 1
 
 NDefines_Graphics.NMapIcons.STATES_PRIORITY_VICTORY_POINTS = 4
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_LOW = 5.0 -- thickness in pixels -- 5
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_COUNTRY_CENTER_THICKNESS = 3.0 -- The center gradient is linear 1/255 per pixel for this many pixels
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 15.0
-NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STATE = 45.0 --11
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_LOW = 10.0 -- thickness in pixels -- 5
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_COUNTRY_CENTER_THICKNESS = 1.0 -- The center gradient is linear 1/255 per pixel for this many pixels
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 30.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STATE = 10.0 --11
 NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_A = 2.0 --2
 NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_SUPPLY_AREA_B = 15.0 --20
 NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STRATEGIC_REGIONS = 50.0 --20
